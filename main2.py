7ed9beade0685d2428d612d0cfa6534d6b8f081a
Enes Güler <kriptoenes@gmail.com>
2022-01-05 13:49:43 +0300

    graph methods added

--- main2.py
+++ main2.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 import sys
 import math
@@ -132,11 +131,75 @@ def angle_of_a_line(p1, p2):
 		return 360 - alpha
 
 
+def shift_graph(graph):
+	# Find min length
+	min_l = 100000
+	min_l_ind = -1
+	for ind, pair in enumerate(graph):
+		a, l = pair
+		if l < min_l:
+			min_l = l
+			min_l_ind = ind
+
+	# Shifting
+	while min_l_ind > -1:
+		pair = graph.pop(0)
+		a, l = pair
+		graph.append((a + 360, l))
+
+		min_l_ind -= 1
+
+
+def smooth_graph(graph):
+	peaks_graph = []
+	i = 1
+	while i < len(graph) - 1:
+		a, l = graph[i]
+		_, l_left = graph[i - 1]
+		_, l_right = graph[i + 1]
+
+		if l > l_left and l > l_right:  # It is a peak
+			peaks_graph.append((a, l))
+		i += 1
+	return peaks_graph
+
+
+def detect_corners(graph):
+	lengths = [l for _, l in graph]
+
+	min_l = min(lengths)
+	max_l = max(lengths)
+	diff = max_l - min_l
+
+	corner_count = 0
+	i = 1
+	last_peak_angle = -1000
+	while i < len(graph) - 1:
+		a, l = graph[i]
+		_, l_left = graph[i - 1]
+		_, l_right = graph[i + 1]
+
+		if l > l_left and l > l_right:  # It is a peak
+			if a - last_peak_angle > 15:
+				pass
+
+
+def show(window_name, img):
+	cv.imshow(window_name, img)
+	cv.waitKey()
+
+
 image = cv.imread("shapes6.jpeg", 0)
-blured = cv.GaussianBlur(image, (7, 7), 0)
+show("image", image)
+
+blured = cv.GaussianBlur(image, (21, 21), 0)
+show("blured", blured)
+
 _, th = cv.threshold(blured, 180, 255, cv.THRESH_BINARY)
+show("th", th)
 
 edges = edge_points(th)
+show("edges", edges)
 
 clusters = cluster_edges(edges)
 print("Number of found shapes:", len(clusters))
@@ -155,7 +218,7 @@ for shape in clusters:
 	angle_axix = [angle_of_a_line(center_point, point) for point in points]
 	l = list(zip(angle_axix, length_axis))
 
-	graph = sorted(l, key=lambda x: x[0])
+	graph = list(sorted(l, key=lambda x: x[0]))
 	# print(graph)
 	# angle_axix.sort()
 	# cv.circle(image, center_point, 6, (255, 0, 0), thickness=-1)
@@ -167,7 +230,8 @@ for shape in clusters:
 	angle_axix = [i for i, j in graph]
 	length_axis = [j for i, j in graph]
 
-	print("min_l:", int(min(length_axis)), "   max_l:", int(max(length_axis)), "   dif:", int(max(length_axis)-min(length_axis)))
+	print("min_l:", int(min(length_axis)), "   max_l:", int(max(length_axis)), "   dif:",
+		  int(max(length_axis) - min(length_axis)))
 	print("---------------------------------------------------------------------------")
 	plt.plot(angle_axix, length_axis)
 
@@ -175,6 +239,33 @@ for shape in clusters:
 	plt.xlabel('angle_axix')
 	plt.title('shape_graph')
 	plt.show()
+	#################################
+	shift_graph(graph)
+
+	angle_axix = [i for i, j in graph]
+	length_axis = [j for i, j in graph]
+
+	plt.plot(angle_axix, length_axis)
+
+	plt.ylabel('length_axis')
+	plt.xlabel('angle_axix')
+	plt.title('shape_graph')
+	plt.show()
+	#################################
+	peaks_graph = smooth_graph(graph)
+
+	angle_axix = [i for i, j in peaks_graph]
+	length_axis = [j for i, j in peaks_graph]
+
+	plt.plot(angle_axix, length_axis)
+
+	plt.ylabel('length_axis')
+	plt.xlabel('angle_axix')
+	plt.title('peaks_graph')
+	plt.show()
+
+#################################
+# detect_corners(peaks_graph)
 
 # cv.imshow("th", th)
 # cv.imshow("edges", edges)
